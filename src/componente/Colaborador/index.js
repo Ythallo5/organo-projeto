f2912d2206cb366b931df9deb611eab990379cca
Gabriel Airton <gabriel.airton@paraiba.email>
2024-05-29 11:29:22 -0300

    projeto

---
+++ src/componente/Colaborador/index.js
@@ -0,0 +1,16 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div>
+      <div>
+        <img src="http://github.com/viniciosneves.png" alt="Vinicios Neves" />
+      </div>
+      <h4>Vinicios Neves</h4>
+      <h5>Instrutor</h5>
+      <div></div>
+    </div>
+  );
+};
+
+export default Colaborador;
